63c9f2c71baa6500e4584ae8c3b70f146ff3b7a6
Pablo Fernandes <veooow@gmail.com>
2024-02-18 23:44:38 -0300

    chore: add els to logs

--- src/main/java/com/example/bank/controllers/TransactionController.java
+++ src/main/java/com/example/bank/controllers/TransactionController.java
@@ -4,6 +4,7 @@ import com.example.bank.domain.transaction.Transaction;
 import com.example.bank.domain.transaction.TransactionType;
 import com.example.bank.dtos.TransactionDTO;
 import com.example.bank.services.TransactionService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +14,16 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/transactions")
+@Slf4j
 public class TransactionController {
     @Autowired
     private TransactionService transactionService;
+
+    @GetMapping("/logs")
+    public String Test() {
+        log.info("Test logs");
+        return "It's just a test";
+    }
 
     @GetMapping
     public ResponseEntity<List<Transaction>> getMyTransactions(@RequestParam(value = "id") Long id, @RequestParam(value = "type") TransactionType type) throws Exception {
@@ -31,6 +39,8 @@ public class TransactionController {
     @PostMapping
     public ResponseEntity<Transaction> createNewTransaction(@RequestBody TransactionDTO transaction) throws Exception {
         Transaction newTransaction = transactionService.createTransaction(transaction);
+
+        log.info("Transaction has made with success!");
         return new ResponseEntity<>(newTransaction, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/example/bank/services/TransactionService.java
+++ src/main/java/com/example/bank/services/TransactionService.java
@@ -4,6 +4,7 @@ import com.example.bank.domain.transaction.Transaction;
 import com.example.bank.domain.user.User;
 import com.example.bank.dtos.TransactionDTO;
 import com.example.bank.repositories.TransactionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @Service
+@Slf4j
 public class TransactionService {
     @Autowired
     private UserService userService;
@@ -49,6 +51,7 @@ public class TransactionService {
 
         boolean isAuthorized = authorizedTransaction(sent, transactionDTO.value());
         if (!isAuthorized) {
+            log.error("Transação não autorizada");
             throw new Exception("Transação não autorizada");
         }
 
