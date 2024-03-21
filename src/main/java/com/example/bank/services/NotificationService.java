71f22c4f67539e34624eb74d9b035c0eb5466123
Pablo Fernandes <veooow@gmail.com>
2024-03-20 23:03:52 -0300

    fix: remove sout from notification service

--- src/main/java/com/example/bank/services/NotificationService.java
+++ src/main/java/com/example/bank/services/NotificationService.java
@@ -2,6 +2,9 @@ package com.example.bank.services;
 
 import com.example.bank.domain.user.User;
 import com.example.bank.dtos.NotificationDTO;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Map;
 
 @Service
+@Slf4j
 public class NotificationService {
     @Autowired
     private RestTemplate restTemplate;
@@ -21,7 +25,7 @@ public class NotificationService {
         ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6", notificationRequest, String.class);
 
         if (!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
-            System.out.println("Erro ao enviar notificação!");
+            log.error("Erro ao enviar notificação!");
             throw new Exception("Serviço de notificação OFF!");
         }
     }
