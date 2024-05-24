a5c0e2ad8f9f6b629d0200b74671b05852e38329
Pablo Fernandes <veooow@gmail.com>
2024-05-24 00:16:03 -0300

    feat: implements JPA paginable

--- src/main/java/com/example/bank/repositories/TransactionRepository.java
+++ src/main/java/com/example/bank/repositories/TransactionRepository.java
@@ -1,18 +1,20 @@
 package com.example.bank.repositories;
 
 import com.example.bank.domain.transaction.Transaction;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     @Query("SELECT t FROM transactions t WHERE t.receiver.id = :id OR t.sent.id = :id")
-    Optional<List<Transaction>> findByReceiverIdOrSentId(@Param("id") Long id);
-    Optional<List<Transaction>> findTransactionByReceiverId(Long id);
-    Optional<List<Transaction>> findTransactionBySentId(Long id);
+    Page<Transaction> findByReceiverIdOrSentId(Long id, Pageable pageable);
+    Page<Transaction> findTransactionByReceiverId(Long id, Pageable pageable);
+    Page<Transaction> findTransactionBySentId(Long id, Pageable pageable);
 }
--- src/main/java/com/example/bank/strategies/transactions/TransactionStrategy.java
+++ src/main/java/com/example/bank/strategies/transactions/TransactionStrategy.java
@@ -1,9 +1,10 @@
 package com.example.bank.strategies.transactions;
 
 import com.example.bank.domain.transaction.Transaction;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface TransactionStrategy {
-    List<Transaction> getTransactions(Long id) throws Exception;
+    Page<Transaction> getTransactions(Long id) throws Exception;
 }
